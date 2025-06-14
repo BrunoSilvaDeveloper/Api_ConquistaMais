8225ef14a3fc0ceaf13f1954890efb8b278e4e13
Bruno Silva Dev <faellbrunim@gmail.com>
2025-06-14 14:20:31 -0300

    api

--- app/Http/Resources/EventoResource.php
+++ app/Http/Resources/EventoResource.php
@@ -8,29 +8,17 @@ class EventoResource extends JsonResource
 {
     public function toArray($request)
     {
-        // Quebra do local_evento
-        [$local, $resto] = explode(',', $this->local_evento);
-        [$cidade, $estado] = explode('-', $resto);
+        $eventoLimpo = str_replace("\u{A0}", ' ', $this->local_evento);
+        $eventoLimpo = preg_replace('/\x{00A0}/u', ' ', $eventoLimpo); // compatibilidade extra
 
-        // Quebra da data_evento
-        [$dataInicioRaw, $dataFimRaw] = explode('>', $this->data_evento);
+        [$local, $resto] = explode(',', $eventoLimpo) + [null, null];
+        [$cidade, $estado] = explode('-', $resto) + [null, null];
 
         $meses = [
             'jan' => '01', 'fev' => '02', 'mar' => '03', 'abr' => '04',
             'mai' => '05', 'jun' => '06', 'jul' => '07', 'ago' => '08',
             'set' => '09', 'out' => '10', 'nov' => '11', 'dez' => '12',
         ];
-
-        function formatarData($dataString, $meses)
-        {
-            preg_match('/(\d{2}) (\w{3}) - (\d{4})/', $dataString, $matches);
-            if (count($matches) === 4) {
-                [$_, $dia, $mesAbreviado, $ano] = $matches;
-                $mes = $meses[strtolower($mesAbreviado)] ?? '01';
-                return "{$dia}/{$mes}/{$ano}";
-            }
-            return null;
-        }
 
         return [
             'id' => $this->id,
@@ -39,11 +27,26 @@ class EventoResource extends JsonResource
             'link_evento' => $this->link_evento,
             'link_imagem' => $this->link_imagem,
             'evento' => $this->evento,
-            'local' => trim($local),
-            'cidade' => trim($cidade),
-            'estado' => trim($estado),
-            'data_inicial' => formatarData($dataInicioRaw, $meses),
-            'data_final' => formatarData($dataFimRaw, $meses),
+            'local' => trim($local ?? ''),
+            'cidade' => trim($cidade ?? ''),
+            'estado' => trim($estado ?? ''),
+            'data_inicial' => $this->formatarData($this->data_evento, $meses, true),
+            'data_final' => $this->formatarData($this->data_evento, $meses, false),
         ];
     }
+
+    private function formatarData($dataCompleta, $meses, $inicio = true)
+    {
+        $partes = explode('>', $dataCompleta);
+        $alvo = $inicio ? ($partes[0] ?? '') : ($partes[1] ?? '');
+
+        preg_match('/(\d{2}) (\w{3}) - (\d{4})/', $alvo, $matches);
+        if (count($matches) === 4) {
+            [$_, $dia, $mesAbreviado, $ano] = $matches;
+            $mes = $meses[strtolower($mesAbreviado)] ?? '01';
+            return "{$dia}/{$mes}/{$ano}";
+        }
+        return null;
+    }
+
 }
